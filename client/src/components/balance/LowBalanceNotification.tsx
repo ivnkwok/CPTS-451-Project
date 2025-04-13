cc2b6ab17811792a0f577f395c6a1a00a3682484
Ivan Kwok <ivnkwok@gmail.com>
2025-04-12 20:18:05 -0700

    Created balance notification (no styling)

---
+++ client/src/components/balance/LowBalanceNotification.tsx
@@ -0,0 +1,9 @@
+const LowBalanceNotification = () => {
+  return (
+    <div className="low-balance-notification">
+      Warning: Your balance is low. Please top up soon.
+    </div>
+  );
+};
+
+export default LowBalanceNotification;

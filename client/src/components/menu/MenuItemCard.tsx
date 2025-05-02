19de04310d64888314096df2a3a9d12ccf14bbd1
Ivan Kwok <ivnkwok@gmail.com>
2025-05-01 18:14:48 -0700

    added input for special instructions to order

--- client/src/components/menu/MenuItemCard.tsx
+++ client/src/components/menu/MenuItemCard.tsx
@@ -108,7 +108,10 @@ const MenuItemCard: React.FC<MenuItemProps> = (props) => {
                 ${Number(price).toFixed(2)}
               </p>
             </div>
-
+            <input 
+              name="notes"
+              placeholder="Enter special instructions..."
+              />
             {/* Show success or error message */}
             {message && <p>{message}</p>}
           </div>
